f1972fc3db1f90158eab7bfde910798f77f3eacd
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2016-01-29 18:38:45 +0000

    Gradle based gdx will probably be easier... (game loop)

---
+++ desktop/src/org/globalgamejam/goldsmiths/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.globalgamejam.goldsmiths.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.globalgamejam.goldsmiths.GoldGame2;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GoldGame2(), config);
+	}
+}
