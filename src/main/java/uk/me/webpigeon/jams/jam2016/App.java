d3a21b65917a54757e7b130ad1dca31db684eaf0
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2016-01-29 20:08:35 +0000

    title screen

--- src/main/java/uk/me/webpigeon/jams/jam2016/App.java
+++ src/main/java/uk/me/webpigeon/jams/jam2016/App.java
@@ -1,5 +1,9 @@
 package uk.me.webpigeon.jams.jam2016;
 
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 /**
  * Hello world!
  *
@@ -12,5 +16,14 @@ public class App
         System.out.println("Strawberry");
         System.out.println("Orange");
         System.out.println("Gooseberry");
+        
+        JFrame frame = new JFrame("Global Game Jam 2016");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
+        
+        frame.add(new TitleScreen());
+        frame.pack();
+        
+        frame.setVisible(true);
     }
 }
---
+++ src/main/java/uk/me/webpigeon/jams/jam2016/TitleScreen.java
@@ -0,0 +1,28 @@
+package uk.me.webpigeon.jams.jam2016;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JComponent;
+
+public class TitleScreen extends JComponent {
+	
+	public TitleScreen(){
+		super();
+		this.setPreferredSize(new Dimension(800, 600));
+	}
+	
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		//TODO render world
+		
+		g.setColor(Color.WHITE);
+		g.drawString("Game Jam 2016", 100, 100);
+	}
+
+}
