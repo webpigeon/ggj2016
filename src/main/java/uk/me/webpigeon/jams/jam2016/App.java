74c2eeab910e68ee8ff90306ab49f63c1c789ff8
Funky Hammer Guy <solar_1992@hotmail.co.uk>
2016-01-29 19:57:09 +0000

    Added some of the jams

---
+++ src/main/java/uk/me/webpigeon/jams/jam2016/App.java
@@ -0,0 +1,16 @@
+package uk.me.webpigeon.jams.jam2016;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Strawberry");
+        System.out.println("Orange");
+        System.out.println("Gooseberry");
+    }
+}
