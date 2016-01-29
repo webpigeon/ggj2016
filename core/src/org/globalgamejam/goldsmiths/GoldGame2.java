9b3a7a603f805f22e8fc0d66fd88110db091e259
Joseph Walton-Rivers <joseph@webpigeon.me.uk>
2016-01-29 19:27:30 +0000

    hide eclipse files from git, art work to use (testing)

--- core/src/org/globalgamejam/goldsmiths/GoldGame2.java
+++ core/src/org/globalgamejam/goldsmiths/GoldGame2.java
@@ -3,25 +3,39 @@ package org.globalgamejam.goldsmiths;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.IsometricTiledMapRenderer;
 
 public class GoldGame2 extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	TiledMap tiledMap;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		
+		tiledMap = new TmxMapLoader().load("test.tmx");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		float unitScale = 1/16f;
+		IsometricTiledMapRenderer renderer = new IsometricTiledMapRenderer(tiledMap);
+		
+		OrthographicCamera camera = new OrthographicCamera();
+		
+		renderer.render();
+		
+		//Gdx.gl.glClearColor(1, 1, 0, 1);
+		//Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		//batch.begin();
+		//batch.draw(img, 0, 0);
+		//batch.end();
 	}
 }
--- desktop/src/org/globalgamejam/goldsmiths/desktop/DesktopLauncher.java
+++ desktop/src/org/globalgamejam/goldsmiths/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import org.globalgamejam.goldsmiths.GoldGame2;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		
 		new LwjglApplication(new GoldGame2(), config);
 	}
 }
